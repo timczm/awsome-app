8099ac7effc8f8055b0c9d2b1e180cfa42142eae
chenzhiming <39296287+timczm@users.noreply.github.com>
2023-08-23 18:01:00 +0800

    remove apikey

--- chatgpt/setup.sh
+++ chatgpt/setup.sh
@@ -8,11 +8,10 @@ apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 echo ""
 echo ""
-read -p "请输入 openai_api_key: " openai_api_key
 read -p "请输入访问密码（建议填写，不填默认为无密码）: " password
 read -p "请输入访问端口（不填默认为80端口）: " port
 port=${port:-80}
-docker run --restart=always -d -p $port:3000 -e OPENAI_API_KEY="$openai_api_key" -e CODE="$password" yidadaa/chatgpt-next-web
+docker run --restart=always -d -p $port:3000 -e CODE="$password" yidadaa/chatgpt-next-web
 public_ip=$(curl -s https://ipinfo.io/ip)
 echo ""
 echo ""
